90bd80d8277634cbd973e8ad5f90cbe38c6a6251
jyotijauhari <jyotijauhari222@gmail.com>
2022-04-15 12:29:17 +0530

    getting innnings

--- scorecard.js
+++ scorecard.js
@@ -51,6 +51,50 @@ function getMatchDetails(html){
     console.log("team 1: " + ownTeam);
     console.log("team 2: " + opponentTeam);
 
+    //5. get innings
+    console.log("Innings: ");
+    let allBatsmenTable = selecTool(".ds-w-full.ds-table.ds-table-xs.ds-table-fixed.ci-scorecard-table > tbody");
+    
+    // console.log(allBatsmenTable.html()); //html of table 1, because two table is there
+    // console.log(allBatsmenTable.length); // 2
+
+    // for(let i=0; i<allBatsmenTable.length;i++){
+    //     let tablehtml = selecTool(allBatsmenTable[i]).html();
+    //     console.log(tablehtml);
+    // }
+
+    //for every table -> get all rows of that table -> find first td span -> because span has special class that empty row doesn't have 
+    for (let i = 0; i < allBatsmenTable.length; i++) {
+        let allRows = selecTool(allBatsmenTable[i]).find("tr"); // all rows arr -> data of batsmen + empty rows 
+        for (let i = 0; i < allRows.length; i++) {
+            let row = selecTool(allRows[i]); // selectool isliye likha qki .find() krna h aage row pe, jo ki direct nhi kr skte , isme tr agya-> jisme bohot saare td hai :) 
+            let firstColmnOfRow = row.find("td")[0]; //first td of tr
+            let spann = selecTool(firstColmnOfRow).find("span"); //getting span of td
+            
+            // skip didnot bat row
+            if(row.hasClass("!ds-border-b-0")){
+                // console.log("did not bat row");
+            }
+
+            //row -> td -> span that have this this class only have data (skipping empty row indirectly)
+            else if(selecTool(spann).hasClass("ds-inline-flex" && "ds-items-center" && "ds-leading-none")){
+                let playerName = selecTool(row.find("td")[0]).text().trim();
+                let runs = selecTool(row.find("td")[2]).text();
+                let balls = selecTool(row.find("td")[3]).text();
+                let numberOf4 = selecTool(row.find("td")[5]).text();
+                let numberOf6 = selecTool(row.find("td")[6]).text();
+                let sr = selecTool(row.find("td")[7]).text();
+
+                console.log(
+                    `${playerName} ->  ${runs} ->  ${balls} -> ${numberOf4} -> ${numberOf6} -> ${sr}`
+                  );
+
+            }
+             
+          }
+      
+      }
+
 }
 
 
